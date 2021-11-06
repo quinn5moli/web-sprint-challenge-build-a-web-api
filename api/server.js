2e29d65a61cb4ead406024333e3e0fd18fb0f039
quinn5moli <quinn.moli@gmail.com>
2021-11-06 15:40:33 -0700

    nodemon,dotenv,colors,cors,morgan added; basice server and route setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const helmet = require('helmet')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const actionsRoutes = require('./actions/actions-router')
+const projectsRoutes = require('./projects/projects-router')
+
+
 const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(morgan('dev'));
+server.use(express.json())
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+// server.use('/api/actions', actionsRoutes)
+// server.use('api/projects', projectsRoutes)
+
 
 module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require('dotenv').config()
+require('colors')
+
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () =>
+console.log(`\n**server is running on port ${PORT}**`.america)
+)
