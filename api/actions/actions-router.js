2435cb280896efa0361d2960aac5746b4fbf8878
quinn5moli <quinn.moli@gmail.com>
2021-11-07 17:54:37 -0800

    basic router scaffolding

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,35 @@
 // Write your "actions" router here!
+// Write your "projects" router here!
+const route = require('express').Router();
+
+const Actions = require('./actions-model');
+
+router.get('/', (req, res) => {
+    Actions.get()
+    .then()
+    .catch()
+})
+
+router.get('/:id', (req, res) => {
+    res.json()
+})
+
+router.post('/', (req, res) => {
+    Actions.insert()
+    .then()
+    .catch()
+})
+
+router.put('/:id', (req, res) => {
+    Actions.update()
+    .then()
+    .catch()
+})
+
+router.delete('/:id', (res, req) => {
+    Actions.remove()
+    .then()
+    .catch()
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,40 @@
 // Write your "projects" router here!
+const route = require('express').Router();
+
+const Projects = require('./projects-model');
+
+router.get('/', (req, res) => {
+    Projects.get()
+    .then()
+    .catch()
+})
+
+router.get('/:id', (req, res) => {
+    res.json()
+})
+
+router.post('/', (req, res) => {
+    Projects.insert()
+    .then()
+    .catch()
+})
+
+router.put('/:id', (req, res) => {
+    Projects.update()
+    .then()
+    .catch()
+})
+
+router.delete('/:id', (res, req) => {
+    Projects.remove()
+    .then()
+    .catch()
+})
+
+router.get('/:id/actions', (req, res) => {
+    Projects.getProjectActions()
+    .then()
+    .catch()
+})
+
+module.exports = router
